c99b94234d7f8a2dcd48b8f26a887634450f166a
exitshadow <exertingshadows@gmail.com>
2022-06-01 16:42:06 +0200

    added shadows

--- js/sketch.js
+++ js/sketch.js
@@ -11,6 +11,10 @@ decor.load(
     'mesh/decor.gltf',
     function (gltf) {
         scene.add(gltf.scene);
+        gltf.scene.traverse(function(obj){
+            obj.castShadow = true;
+            obj.receiveShadow = true;
+        })
     },
     function (xhr){
         console.log((xhr.loaded / xhr.total * 100) + "% loaded");
@@ -27,7 +31,10 @@ jabuticaba.load(
         scene.add(gltf.scene);
         gltf.scene.position.y = 2.4;
         gltf.scene.position.x = 8;
-        machin = gltf.scene;
+        gltf.scene.traverse(function(obj){
+            obj.castShadow = true;
+            obj.receiveShadow = true;
+        })
     },
     function (xhr){
         console.log((xhr.loaded / xhr.total * 100) + "% loaded");
@@ -39,29 +46,47 @@ jabuticaba.load(
 
 var pc = createCarac("Stevie", "img/stevie_heart.png");
     
-var pointLight1 = new THREE.PointLight(0xfff146, 2, 50);
-pointLight1.position.set(3, 2, -5);
+var pointLight1 = new THREE.PointLight(0xfff146, .2, 50);
+pointLight1.position.set(-10, 8, -5);
 
-var pointLight2 = new THREE.PointLight(0x94bbe9, .3, 50);
-pointLight2.position.set(-15, 1, -8);
+var pointLight2 = new THREE.PointLight(0x94bbe9, .1, 50);
+pointLight2.position.set(-8, 3, -12);
 
 var ambientLight = new THREE.AmbientLight(0xfc7070, .6);
 
-scene.add(pc.charSprite);
-// the character constructor bridges all previous functions inside of itself
-    // information to build the character’s sprite
-    // const charTex = new THREE.TextureLoader().load(pc.getSkin().src);
-    // const charMaterial = new THREE.SpriteMaterial( {map: charTex});
-    // const charSprite = new THREE.Sprite(charMaterial);
+// light setup for shadows
+const directLight = new THREE.DirectionalLight(0xffa000, .5);
+directLight.position.y = 8;
+directLight.position.x = 5;
+directLight.position.z = -5;
 
+scene.add(pc.charSprite);
 scene.add(ambientLight);
 scene.add(pointLight1);
 scene.add(pointLight2);
-
+scene.add(directLight);
 scene.add(camera);
 
 var renderer = new THREE.WebGLRenderer({alpha: true});
 renderer.setSize(innerWidth, innerHeight);
+
+// shadow setup
+renderer.shadowMap.enabled = true;
+
+directLight.castShadow = true;
+directLight.shadow.bias = -.001;
+directLight.shadow.camera.zoom = .5;
+
+pointLight1.castShadow = true;
+pointLight1.shadow.bias = -.001;
+pointLight1.shadow.camera.zoom = .5;
+
+pointLight2.castShadow = true;
+pointLight2.shadow.bias = -.001;
+pointLight2.shadow.camera.zoom = .5;
+
+
+
 
 const controls = new THREE.OrbitControls(camera, renderer.domElement);
 
